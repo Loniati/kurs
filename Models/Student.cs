f84ec4ece1c627d823d615a6e55bb1a7d33deff4
Loniati <demidzilov@gmail.com>
2026-06-24 15:39:47 +0300

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication1.Models;
+
+public class Student
+{
+    public string Name { get; set; } = string.Empty;
+    public string Group { get; set; } = string.Empty;
+    public string ScientificAdvisor { get; set; } = string.Empty;
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using AvaloniaApplication1.Models;
+
+namespace AvaloniaApplication1.ViewModels;
+public partial class MainWindowViewModel : ObservableObject
+{
+    public ObservableCollection<Student> Students { get; } = new();
+
+    [ObservableProperty] private Student? _selectedStudent;
+    [ObservableProperty] private string _currentName = "";
+    [ObservableProperty] private string _currentGroup = "";
+    [ObservableProperty] private string _currentAdvisor = "";
+
+    [RelayCommand]
+    private void Add()
+    {
+        Students.Add(new Student { Name = CurrentName, Group = CurrentGroup, ScientificAdvisor = CurrentAdvisor });
+    }
+
+    [RelayCommand]
+    private void Delete()
+    {
+        if (SelectedStudent != null) Students.Remove(SelectedStudent);
+    }
+}
